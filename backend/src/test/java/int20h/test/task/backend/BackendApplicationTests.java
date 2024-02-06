deef32874b257810f903a7fcd3e2d38be10e7da3
momopoi443 <myzika99993824@gmail.com>
2024-02-06 15:51:36 +0200

    init commit

---
+++ backend/src/test/java/int20h/test/task/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package int20h.test.task.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
